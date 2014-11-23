d714311ba1492c125bc8816c18228d7b4383c613
Sara Williamson <sara.e.williamson@gmail.com>
2014-11-22 23:50:39 -0500

    can now search twitter by longest word in substring

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains some of my early NaNoGenMo experiments as well as my main program,
+_Wuthering Tweets_. I'm trying to replace what the characters in Emily Bronte's _Wuthering Heights_
+say with (semi) relevant tweets.
--- regex-practice.rb
+++ regex-practice.rb
@@ -1,3 +1,16 @@
+require 'twitter'
+load 'twitter_config.rb'
+
 test_string = '"I went to the store to get some puppies," she said excitedly. "There were so many!"'
 
-puts test_string.gsub(/(["'])(?:(?=(\\?))\2.)*?\1/, '"PUPPIES PUPPIES PUPPIES"')
+substring = test_string.slice(/(["'])(?:(?=(\\?))\2.)*?\1/)
+
+def longest(string)
+  string.split(" ").max_by(&:length)
+end
+
+search_term = longest(substring)
+
+tweet = @client.search(search_term, result_type: "popular").first.text
+
+puts test_string.gsub(/(["'])(?:(?=(\\?))\2.)*?\1/, "\"#{tweet.delete("\n")}\"")
